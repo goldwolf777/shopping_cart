1138a5715b87e727fcd9160fa2fcaff2e08bcf0c
Ilja Ganulevics <i.ganulevics@gmail.com>
2018-09-09 09:48:56 +0100

    STEP2 - Adding tests for the buy one get one free apples and buy three for a price of two oranges. Cleaning the ShoppingCartCalculations.java

--- src/main/java/com/capgemini/test/calculations/ShoppingCartCalculations.java
+++ src/main/java/com/capgemini/test/calculations/ShoppingCartCalculations.java
@@ -10,14 +10,16 @@ public class ShoppingCartCalculations {
 	private final Logger LOG = LoggerFactory.getLogger(ShoppingCartCalculations.class);
 	private static final String APPLE = "Apple";
 	private static final String ORANGE = "Orange";
+	private static final double APPLE_PRICE = 0.60;
+	private static final double ORANGE_PRICE = 0.25;
 
 	public double calculatePrice(List<String> shoppingList) {
 		double totalPrice = 0.00;
 		for (String item : shoppingList) {
 			if (item.equalsIgnoreCase(APPLE)) {
-				totalPrice += 0.60;
+				totalPrice += APPLE_PRICE;
 			} else if (item.equalsIgnoreCase(ORANGE)) {
-				totalPrice += 0.25;
+				totalPrice += ORANGE_PRICE;
 			} else {
 				LOG.info("Item in the shopping list is unknown: {}", item);
 			}
--- src/test/java/com/capgemini/test/ShoppingCartCalculationsTest.java
+++ src/test/java/com/capgemini/test/ShoppingCartCalculationsTest.java
@@ -8,34 +8,84 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ShoppingCartCalculationsTest {
 
-	@Test
-	void calculateThePriceForOneApple() {
-		ShoppingCartCalculations shoppingCartCalculations = new ShoppingCartCalculations();
-		assertEquals(0.60, shoppingCartCalculations.calculatePrice(asList("Apple")));
-	}
-
-	@Test
-	void calculateThePriceForOneOrange() {
-		ShoppingCartCalculations shoppingCartCalculations = new ShoppingCartCalculations();
-		assertEquals(0.25, shoppingCartCalculations.calculatePrice(asList("Orange")));
-	}
-
-	@Test
-	void calculateThePriceForOneOrangeAndOneApple() {
-		ShoppingCartCalculations shoppingCartCalculations = new ShoppingCartCalculations();
-		assertEquals(0.85, shoppingCartCalculations.calculatePrice(asList("Orange", "Apple")));
-	}
-
-	@Test
-	void calculateThePriceForMultipleApplesAndOranges() {
-		ShoppingCartCalculations shoppingCartCalculations = new ShoppingCartCalculations();
-		assertEquals(1.70, shoppingCartCalculations.calculatePrice(asList("Orange", "Apple", "Apple", "Orange")));
-	}
+//	@Test
+//	void calculateThePriceForOneApple() {
+//		ShoppingCartCalculations shoppingCartCalculations = new ShoppingCartCalculations();
+//		assertEquals(0.60, shoppingCartCalculations.calculatePrice(asList("Apple")));
+//	}
+//
+//	@Test
+//	void calculateThePriceForOneOrange() {
+//		ShoppingCartCalculations shoppingCartCalculations = new ShoppingCartCalculations();
+//		assertEquals(0.25, shoppingCartCalculations.calculatePrice(asList("Orange")));
+//	}
+//
+//@Test
+//void calculateThePriceForOneOrangeAndOneApple() {
+//	ShoppingCartCalculations shoppingCartCalculations = new ShoppingCartCalculations();
+//	assertEquals(0.85, shoppingCartCalculations.calculatePrice(asList("Orange", "Apple")));
+//}
+//
+//	@Test
+//	void calculateThePriceForMultipleApplesAndOranges() {
+//		ShoppingCartCalculations shoppingCartCalculations = new ShoppingCartCalculations();
+//		assertEquals(1.70, shoppingCartCalculations.calculatePrice(asList("Orange", "Apple", "Apple", "Orange")));
+//	}
 
 	@Test
 	void shouldntCalculateUnknownItem() {
 		ShoppingCartCalculations shoppingCartCalculations = new ShoppingCartCalculations();
 		assertEquals(0.00, shoppingCartCalculations.calculatePrice(asList("Applllle")));
 	}
+
+
+	@Test
+	void calculateBuyOneAppleGetOneFree() {
+		ShoppingCartCalculations ShoppingCartCalculations = new ShoppingCartCalculations();
+		assertEquals(0.60, ShoppingCartCalculations.calculatePrice(asList("Apple", "Apple")));
+	}
+
+	@Test
+	void calculateBuyOneAppleGetOneFreeThreeApples() {
+		ShoppingCartCalculations ShoppingCartCalculations = new ShoppingCartCalculations();
+		assertEquals(1.20, ShoppingCartCalculations.calculatePrice(asList("Apple", "Apple", "Apple")));
+	}
+
+	@Test
+	void calculateBuyOneAppleGetOneFreeFourApples() {
+		ShoppingCartCalculations ShoppingCartCalculations = new ShoppingCartCalculations();
+		assertEquals(1.20, ShoppingCartCalculations.calculatePrice(asList("Apple", "Apple", "Apple", "Apple")));
+	}
+
+	@Test
+	void calculateThreeForAPriceOfTwoWithThreeOranges() {
+		ShoppingCartCalculations ShoppingCartCalculations = new ShoppingCartCalculations();
+		assertEquals(0.50, ShoppingCartCalculations.calculatePrice(asList("Orange", "Orange", "Orange")));
+	}
+
+	@Test
+	void calculateThreeForAPriceOfTwoWithTwoOranges() {
+		ShoppingCartCalculations ShoppingCartCalculations = new ShoppingCartCalculations();
+		assertEquals(0.50, ShoppingCartCalculations.calculatePrice(asList("Orange", "Orange")));
+	}
+
+	@Test
+	void calculateThreeForAPriceOfTwoWithFiveOranges() {
+		ShoppingCartCalculations ShoppingCartCalculations = new ShoppingCartCalculations();
+		assertEquals(1, ShoppingCartCalculations.calculatePrice(asList("Orange", "Orange", "Orange", "Orange", "Orange")));
+	}
+
+	@Test
+	void calculateThreeForAPriceOfTwoWithSixOranges() {
+		ShoppingCartCalculations ShoppingCartCalculations = new ShoppingCartCalculations();
+		assertEquals(1, ShoppingCartCalculations.calculatePrice(asList("Orange", "Orange", "Orange", "Orange", "Orange", "Orange")));
+	}
+
+	@Test
+	void calculateThreeForAPriceOfTwoWithThreeOrangesAndBuyOneGetOneFreeApple() {
+		ShoppingCartCalculations ShoppingCartCalculations = new ShoppingCartCalculations();
+		assertEquals(1.10, ShoppingCartCalculations.calculatePrice(asList("Orange", "Orange", "Orange", "Apple", "Apple")));
+	}
+
 
 }
